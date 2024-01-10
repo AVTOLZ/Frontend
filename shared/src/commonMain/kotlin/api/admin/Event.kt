339f731e826748a811fec8172d644ae9e2bd9fec
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-01-10 22:32:18 +0100

    changes changes

--- shared/src/commonMain/kotlin/api/admin/Event.kt
+++ shared/src/commonMain/kotlin/api/admin/Event.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Event(
+    val id: Int,
     val requiredRank: AVTRanks,
     var startTime: Long,
     var endTime: Long,
--- shared/src/commonMain/kotlin/ui/admin/children/events/EventsScreen.kt
+++ shared/src/commonMain/kotlin/ui/admin/children/events/EventsScreen.kt
@@ -5,17 +5,22 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import api.admin.Event
 import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
 
 @Composable
 fun EventsScreen(component: EventsComponent) {
     val events = component.events.subscribeAsState().value
+
+    var checked by remember { mutableStateOf(listOf<Event>()) }
 
     val column1Weight = .3f // 30%
     val column2Weight = .7f // 70%
@@ -23,16 +28,29 @@ fun EventsScreen(component: EventsComponent) {
     LazyColumn(Modifier.fillMaxSize().padding(16.dp)) {
         // Here is the header
         item {
-            Row(Modifier.background(androidx.compose.ui.graphics.Color.Gray)) {
-                TableCell(text = "Title", weight = column1Weight)
-                TableCell(text = "Start Time", weight = column2Weight)
+            Row(Modifier.background(Color.Gray), verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(checked = checked.isNotEmpty(), onCheckedChange = {
+                    checked = if (it) events
+                    else emptyList()
+                }, modifier = Modifier.size(20.dp))
+
+                TableCell(weight = .1f) {}
+                TableCell(weight = .3f) { Text("Name") }
+                TableCell(weight = .5f) { Text("Description") }
             }
         }
         // Here are all the lines of your table.
         items(events) { event ->
-            Row(Modifier.fillMaxWidth()) {
-                TableCell(text = event.title, weight = column1Weight)
-                TableCell(text = event.startTime.toString(), weight = column2Weight)
+            Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(checked = checked.contains(event), onCheckedChange = {
+                    checked = if (it) checked + event
+                    else checked.filter { it != event }
+                }, modifier = Modifier.size(20.dp))
+
+                Spacer(Modifier.size(3.dp))
+
+                TableCell(weight = .3f) { Text(event.title) }
+                TableCell(weight = .5f) { Text(event.description) }
             }
         }
     }
@@ -40,14 +58,14 @@ fun EventsScreen(component: EventsComponent) {
 
 @Composable
 fun RowScope.TableCell(
-    text: String,
-    weight: Float
+    modifier: Modifier = Modifier,
+    weight: Float,
+    content: @Composable () -> Unit
 ) {
-    Text(
-        text = text,
-        Modifier
-            .border(Dp.Hairline, Color.Gray)
-            .weight(weight)
-            .padding(4.dp)
-    )
+    Box(Modifier
+        .border(Dp.Hairline, Color.Gray)
+        .weight(weight)
+        .padding(4.dp)
+        .then(modifier)
+    ) { content() }
 }
